375292a2feec286afd95a8e11f0a1984d065da6e
Akira TAGOH <akira@tagoh.org>
2024-05-08 18:10:20 +0900

    Run fontquery-container with correct path
    Fixes #7

--- fontquery/client.py
+++ fontquery/client.py
@@ -1,5 +1,5 @@
-# client.py
-# Copyright (C) 2022-2023 Red Hat, Inc.
+# container.py
+# Copyright (C) 2022 Red Hat, Inc.
 #
 # Authors:
 #   Akira TAGOH  <tagoh@redhat.com>
@@ -21,210 +21,223 @@
 # FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
 # ON AN "AS IS" BASIS, AND THE COPYRIGHT HOLDER HAS NO OBLIGATION TO
 # PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-"""Module to perform a client application for fontquery."""
+"""Module to deal with container for fontquery."""
 
 import argparse
-import importlib.metadata
-import os
+import csv
+import json
+import langtable
 import re
 import shutil
 import subprocess
 import sys
-import tempfile
-import warnings
+import types
+import os
 from collections import Counter
+from pathlib import Path
 try:
     import fontquery_debug # noqa: F401
 except ModuleNotFoundError:
     pass
-import fontquery.htmlformatter # noqa: F401
+from fontquery import version
 try:
-    from fontquery import container  # noqa: F401
-    local_not_supported = False
+    from pyanaconda import localization
+    defaultLangList = [
+        lang for lang in localization.get_available_translations()
+    ]
 except ModuleNotFoundError:
-    local_not_supported = True
-from fontquery.cache import FontQueryCache # noqa: F401
-from pathlib import Path
-from xdg import BaseDirectory
+    defaultLangList = [
+        'aa', 'ab', 'af', 'ak', 'am', 'an', 'ar', 'as', 'ast', 'av', 'ay',
+        'az_az', 'az_ir', 'ba', 'be', 'ber_dz', 'ber_ma', 'bg', 'bh', 'bho',
+        'bi', 'bin', 'bm', 'bn', 'bo', 'br', 'brx', 'bs', 'bua', 'byn', 'ca',
+        'ce', 'ch', 'chm', 'chr', 'co', 'crh', 'cs', 'csb', 'cu', 'cv', 'cy',
+        'da', 'de', 'doi', 'dv', 'dz', 'ee', 'el', 'en', 'eo', 'es', 'et',
+        'eu', 'fa', 'fat', 'ff', 'fi', 'fil', 'fj', 'fo', 'fr', 'fur', 'fy',
+        'ga', 'gd', 'gez', 'gl', 'gn', 'gu', 'gv', 'ha', 'haw', 'he', 'hi',
+        'hne', 'ho', 'hr', 'hsb', 'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie',
+        'ig', 'ii', 'ik', 'io', 'is', 'it', 'iu', 'ja', 'jv', 'ka', 'kaa',
+        'kab', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'kok', 'kr', 'ks',
+        'ku_am', 'ku_iq', 'ku_ir', 'ku_tr', 'kum', 'kv', 'kw', 'kwm', 'ky',
+        'la', 'lah', 'lb', 'lez', 'lg', 'li', 'ln', 'lo', 'lt', 'lv', 'mai',
+        'mg', 'mh', 'mi', 'mk', 'ml', 'mn_cn', 'mn_mn', 'mni', 'mo', 'mr',
+        'ms', 'mt', 'my', 'na', 'nb', 'nds', 'ne', 'ng', 'nl', 'nn', 'no',
+        'nqo', 'nr', 'nso', 'nv', 'ny', 'oc', 'om', 'or', 'os', 'ota', 'pa',
+        'pa_pk', 'pap_an', 'pap_aw', 'pes', 'pl', 'prs', 'ps_af', 'ps_pk',
+        'pt', 'qu', 'quz', 'rm', 'rn', 'ro', 'ru', 'rw', 'sa', 'sah', 'sat',
+        'sc', 'sco', 'sd', 'se', 'sel', 'sg', 'sh', 'shs', 'si', 'sid', 'sk',
+        'sl', 'sm', 'sma', 'smj', 'smn', 'sms', 'sn', 'so', 'sq', 'sr', 'ss',
+        'st', 'su', 'sv', 'sw', 'syr', 'ta', 'te', 'tg', 'th', 'ti_er',
+        'ti_et', 'tig', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw', 'ty',
+        'tyv', 'ug', 'uk', 'und_zmth', 'und_zsye', 'ur', 'uz', 've', 'vi',
+        'vo', 'vot', 'wa', 'wal', 'wen', 'wo', 'xh', 'yap', 'yi', 'yo', 'za',
+        'zh_cn', 'zh_hk', 'zh_mo', 'zh_sg', 'zh_tw', 'zu'
+    ]
 
 
-def run(release, args):
-    if args.product == 'centos':
-        if re.match(r'\d+(\-development)?$', release):
-            release = 'stream' + release
-    if release == 'local':
-        fqcexec = 'fontquery-container'
-        if not shutil.which(fqcexec):
-            fqcexec = container.__file__
-        cmdline = ['python', fqcexec, '-m', args.mode] + (
-            ['-' + ''.join(['v' * (args.verbose - 1)])] if args.verbose > 1
-            else []) + ([] if args.lang is None else
-                        [' '.join(['-l=' + ls
-                                   for ls in args.lang])]) + args.args
-    else:
-        print('* This may take some time...', file=sys.stderr)
-        cmdline = [
-            'podman', 'run', '--rm',
-            'ghcr.io/fedora-i18n/fontquery/{}/{}:{}'.format(
-                args.product, args.target, release), '-m', args.mode
-        ] + (['-' + ''.join(['v' * (args.verbose - 1)])] if args.verbose > 1
-             else []) + ([] if args.lang is None else
-                         [' '.join(['-l=' + ls
-                                    for ls in args.lang])]) + args.args
+def dump(params: object) -> str:
+    """Dump fontquery result in JSON."""
+    p = Path('/etc/os-release')
+    with open(p) as f:
+        reader = csv.reader(f, delimiter='=')
+        os_release = dict(reader)
+    langname = {
+        lang:
+        langtable.language_name(languageId=re.sub(r'_([a-zA-Z]*)$',
+                                                  lambda r: r.group(0).upper(),
+                                                  lang.replace('-', '_')),
+                                languageIdQuery='en')
+        for lang in params.lang
+    }
+    fqver = version.fontquery_version()
+    if not shutil.which('fc-match'):
+        print('fc-match is not installed', file=sys.stderr)
+        sys.exit(1)
+    jsons = {
+        'id': os_release['ID'],
+        'version_id': os_release['VERSION_ID'],
+        'pattern': params.pattern,
+        'fq_id': fqver,
+        'fonts': [],
+    }
+    for ls in params.lang:
+        for f in params.family:
+            cmdline = [
+                'fc-match', '-f',
+                ('%{file:-<unknown filename|basename},'
+                 '%{family[0]:-<unknown family>},'
+                 '%{style[0]:-<unknown style>}\\n'),
+                '{}:lang={}'.format(f, ls.replace('_','-'))
+            ]
+            if params.verbose:
+                print('# ' + ' '.join(cmdline), flush=True, file=sys.stderr)
+            retval = subprocess.run(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    if args.verbose:
-        print('# ' + ' '.join(cmdline), file=sys.stderr)
+            cond_empty = re.compile(r'^$')
+            out = [
+                s for s in retval.stdout.decode('utf-8').split('\n')
+                if not cond_empty.match(s)
+            ]
+            data = [item.split(',') for item in out][0]
+            jsons['fonts'].append({
+                'lang': ls,
+                'lang_name': langname[ls],
+                'alias': f,
+                'file': data[0],
+                'family': data[1],
+                'style': data[2]
+            })
 
-    result = subprocess.run(cmdline, stdout=subprocess.PIPE)
-    if result.returncode != 0:
-        sys.tracebacklimit = 0
-        raise RuntimeError('`podman run\' failed with the error code {}'.format(result.returncode))
+    return json.dumps(jsons, indent=4)
 
-    return result.stdout.decode('utf-8')
+def checkupdate(params: object) -> str:
+    if not shutil.which('fontquery-setup.sh'):
+        print('fontquery-setup.sh is not installed')
+        sys.exit(1)
+    cmdline = [
+        'fontquery-setup.sh', '-c'
+    ]
+    if params.verbose:
+        print('# ' + ' '.join(cmdline), flush=True, file=sys.stderr)
+    res = subprocess.run(cmdline)
+    sys.exit(res.returncode)
 
-def load(release, args, fcache):
-    out = None
-
-    if release == 'local':
-        out = run(release, args)
-    else:
-        fqc = FontQueryCache(args.product, release, args.target)
-        if args.clean_cache:
-            fqc.delete()
-        if fcache:
-            if args.verbose:
-                print('* Reading JSON from cache', file=sys.stderr)
-            out = fqc.read()
-        if not out:
-            out = run(release, args)
-            if fcache:
-                if args.verbose:
-                    print('* Storing cache...', file=sys.stderr, end='')
-                if fqc.save(out):
-                    if args.verbose:
-                        print('done', file=sys.stderr)
-                else:
-                    if args.verbose:
-                        print('failed', file=sys.stderr)
-
-    return out
+def update(params: object) -> str:
+    if not shutil.which('fontquery-setup.sh'):
+        print('fontquery-setup.sh is not installed')
+        sys.exit(1)
+    cmdline = [
+        'fontquery-setup.sh', '-u'
+    ]
+    if params.verbose:
+        print('# ' + ' '.join(cmdline), flush=True, file=sys.stderr)
+    res = subprocess.run(cmdline)
+    sys.exit(res.returncode)
 
 def main():
-    """Endpoint to execute fontquery client program."""
-    defrel = ['local']
+    """Endpoint to execute fontquery-container program."""
+    fccmd = {'fcmatch': 'fc-match',
+             'fclist': 'fc-list',
+             'json': dump,
+             'update': update,
+             'checkupdate': checkupdate,
+             }
+    fclang_ll_cc = [
+        'az_az', 'az_ir', 'ber_dz', 'ber_ma', 'ku_am', 'ku_iq', 'ku_ir',
+        'ku_tr', 'mn_cn', 'mn_mn', 'pa_pk', 'pap_an', 'pap_aw', 'ps_af',
+        'ps_pk', 'ti_er', 'ti_et', 'und_zmth', 'und_zsye', 'zh_cn', 'zh_hk',
+        'zh_mo', 'zh_sg', 'zh_tw'
+    ]
+    families = ['sans-serif', 'serif', 'monospace']
+    fclangs = []
+    for lang in defaultLangList:
+        added = False
+        for ls in fclang_ll_cc:
+            ll = re.sub('_.*', '', ls)
+            if lang == ll:
+                fclangs.append(ls.replace('_', '-'))
+                added = True
+        if not added:
+            fclangs.append(lang)
 
     parser = argparse.ArgumentParser(
         description='Query fonts',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-C',
-                        '--clean-cache',
-                        action='store_true',
-                        help='Clean caches before processing')
-    parser.add_argument('--disable-cache',
-                        action='store_true',
-                        help='Enforce processing everything even if not updating')
+
     parser.add_argument('-f',
-                        '--filename-format',
-                        default='{product}-{release}-{target}.{mode}',
-                        help='Output filename format. only take effects with --mode=html or using multiple --release option')
-    parser.add_argument('-r',
-                        '--release',
-                        default=defrel,
+                        '--family',
                         action='append',
-                        help='Release number such as "rawhide" and "39". "local" to query from current environment instead of images')
+                        default=families,
+                        help='Families to dump fonts data into JSON')
     parser.add_argument('-l',
                         '--lang',
                         action='append',
+                        default=fclangs,
                         help='Language list to dump fonts data into JSON')
     parser.add_argument('-m',
                         '--mode',
                         default='fcmatch',
-                        choices=['fcmatch', 'fclist', 'json', 'html'],
+                        choices=list(fccmd.keys()),
                         help='Action to perform for query')
-    parser.add_argument('-O',
-                        '--output-dir',
-                        default='.',
-                        help='Output directory')
-    parser.add_argument('-P',
-                        '--product',
-                        default='fedora',
-                        choices=['fedora', 'centos'],
-                        help='Product name to operate')
-    parser.add_argument('-t',
-                        '--target',
-                        default='minimal',
-                        choices=['minimal', 'extra', 'all'],
-                        help='Query fonts from')
-    parser.add_argument('-T',
-                        '--title',
-                        default='{product} {release}: {target}',
-                        help='Page title format. only take effects with --mode=html')
-    parser.add_argument('-v',
-                        '--verbose',
-                        action='count',
-                        default=0,
-                        help='Show more detailed logs')
+    parser.add_argument('-p',
+                        '--pattern',
+                        help='Query pattern to identify fonts data into JSON')
     parser.add_argument('-V',
                         '--version',
                         action='store_true',
-                        help='Show version')
-    parser.add_argument('args', nargs='*', help='Queries')
+                        help='Show image version')
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='store_true',
+                        help='Show more detailed logs')
+    parser.add_argument('args',
+                        nargs='*',
+                        help='Arguments to the corresponding action')
 
     args = parser.parse_args()
-    rr = Counter(args.release)
-    rr.subtract(defrel)
-    rellist = list(rr.elements())
-    args.release = rellist if rellist else defrel
 
-    cache = None
-    cmdline = []
-    out = None
-    origmode = args.mode
-    redirect = True if args.mode == 'html' else False
-    if redirect:
-        args.mode = 'json'
     if args.version:
-        print(importlib.metadata.version('fontquery'))
+        print(version.fontquery_version())
         sys.exit(0)
-    ofile = str(Path(args.output_dir) / args.filename_format)
-    if args.verbose:
-        print('* Product: {}'.format(args.product), file=sys.stderr)
-        print('* Release: {}'.format(args.release), file=sys.stderr)
-        print('* Target: {}'.format(args.target), file=sys.stderr)
-        print('* Language: {}'.format(args.lang), file=sys.stderr)
-        print('* Mode: {}'.format(args.mode), file=sys.stderr)
-        print('* Output: {}'.format(ofile), file=sys.stderr)
-
-    if 'local' in args.release:
-        if local_not_supported:
-            raise TypeError('local query feature is not available.')
-        if args.target != parser.get_default('target'):
-            warnings.warn("target option won't take any effects on local mode", RuntimeWarning, stacklevel=2)
+    # Pick up langs only set by args
+    ll = Counter(args.lang)
+    ll.subtract(fclangs)
+    langlist = list(ll.elements())
+    args.lang = langlist if langlist else fclangs
+    if isinstance(fccmd[args.mode], types.FunctionType):
+        print(fccmd[args.mode](args))
     else:
-        if not shutil.which('podman'):
-            print('podman is not installed')
+        if not shutil.which(fccmd[args.mode]):
+            print('{} is not installed'.format(fccmd[args.mode]),
+                  file=sys.stderr)
             sys.exit(1)
 
-    for r in args.release:
-        out = load(r, args, not args.lang and not args.disable_cache and args.mode == 'json')
-        if redirect:
-            with tempfile.NamedTemporaryFile(mode='w+') as tmp:
-                tmp.write(out)
-                tmp.seek(0)
-                with open(ofile.format(product=args.product, release=r,
-                                       target=args.target, mode=origmode), 'w') as fw:
-                    fontquery.htmlformatter.run('table', tmp, None, fw, fontquery.htmlformatter.HtmlRenderer(),
-                                                args.title.format(product=args.product,
-                                                                  release=r,
-                                                                  target=args.target))
-        else:
-            if len(args.release) == 1:
-                print(out)
-            else:
-                with open(ofile.format(product=args.product, release=r,
-                                       target=args.target, mode=origmode), 'w') as fw:
-                    fw.write(out)
+        if args.lang != fclangs:
+            print('W: lang option does not take any effects. Please use :lang fcpattern instead', flush=True, file=sys.stderr)
+        cmdline = [fccmd[args.mode]] + args.args
+        if args.verbose:
+            print('# ' + ' '.join(cmdline), flush=True, file=sys.stderr)
 
+        subprocess.run(cmdline)
 
 if __name__ == '__main__':
     main()
